58168121982092d9b39d06092f62beb135a2bc99
ZoyirovMuhammadali <muhammadalizoyirov777@gmail.com>
2022-01-11 23:56:17 +0500

    commit

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'weatherapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weatherapp',
 ]
 
 MIDDLEWARE = [
