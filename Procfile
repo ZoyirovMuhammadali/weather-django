a6e79fcc77a9d0280bb4faec6312b57992e4498b
ZoyirovMuhammadali <muhammadalizoyirov777@gmail.com>
2022-01-11 22:46:01 +0500

    onlaynga yuklash

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn weatherproject.wsgi --log-file -
--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r6jqbedv6#p_*7ro=gdf9#%js#v6!1hqxjw7n+gch+de*%g&9m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
